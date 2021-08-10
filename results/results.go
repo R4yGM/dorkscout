ba4a53613086be01fb8fdd53de9756581fa9283b
R4yan <yessou.rayan@gmail.com>
2021-08-10 20:26:47 +0200

    Add files via upload

--- results/results.go
+++ results/results.go
@@ -13,6 +13,87 @@ var (
         timeFinish time.Time
 )
 
+var tpl = `<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Dorkscout results</title>
+  <meta name="description" content="Dorkscout results">
+  <meta name="author" content="Dorkscout">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+  <style>
+    body {
+      margin: 1.5em auto;
+      font-family: "Helvetica Neue", sans-serif;
+      color: #575553;
+      max-width: 1000px;
+      padding: 0 1em;
+    }
+
+    h1 {
+      font-size: 1.75em;
+      color: #003e54;
+      font-weight: bold;
+      line-height: 1.4;
+      margin: 0 0 0.8em 0;
+    }
+
+    .item {
+      padding: 1em;
+      border-bottom: 1px solid #f0f0f0;
+    }
+
+    .dorkscout-item {
+      background-color: #f0f0f0;
+    }
+
+    p {
+      font-size: 1em;
+      line-height: 1.6;
+      margin: 0 0 0.3em 0;
+    }
+
+    p:last-of-type {
+      margin: 0;
+    }
+
+    span.divider-dot {
+      margin: 0 0.3em;
+      color: #999794;
+    }
+
+    .handle {
+      font-weight: bold;
+      color: #000000;
+    }
+
+    a {
+      color: #2d9eb2;
+      text-decoration: none;
+    }
+
+    a:active,
+    a:hover {
+      color: #207180;
+    }
+
+    img {
+      border-radius: 4px;
+    }
+  </style>
+  <meta name="user-style-sheet" content="pdf.css">
+</head>
+<body>
+  <h1 style="color:#000000">Dorkscout results {{.Target}}</h1>
+  {{.Time}}
+  {{.Next}}
+</body>
+</html>`
+
 func InitHTMLResults(path string, target string) {
 
         timeStart = time.Now()
@@ -27,7 +108,7 @@ func InitHTMLResults(path string, target string) {
         vars["Next"] = "{{.Next}}"
         vars["Time"] = timeStart.Format(time.RFC850)
 
-        tmpl, _ := template.ParseFiles("results/template.tpl")
+        tmpl, _ := template.New("template").Parse(tpl)
 
         file, _ := os.Create(path)
         defer file.Close()
@@ -42,7 +123,7 @@ func HTMLFinish(path string) {
 
         vars["Next"] = fmt.Sprintf("Finished scanning at %s, time elapsed %s", timeFinish.Format(time.RFC850), time.Since(timeStart))
 
-        tmpl, _ := template.ParseFiles(path)
+        tmpl, _ := template.New("template").Parse(tpl)
 
         file, _ := os.Create(path)
         defer file.Close()
@@ -75,7 +156,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
                         vars["NextResult"] = result
                         vars["Next"] = "{{.Next}}"
 
-                        tmpl, _ := template.ParseFiles(path)
+                        tmpl, _ := template.New("template").Parse(tpl)
 
                         file, _ := os.Create(path)
                         defer file.Close()
@@ -104,7 +185,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
 
                         vars["Next"] = result
 
-                        tmpl, _ := template.ParseFiles(path)
+                        tmpl, _ := template.New("template").Parse(tpl)
 
                         file, _ := os.Create(path)
                         defer file.Close()
@@ -130,7 +211,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
                         vars["NextResult"] = result
                         vars["Next"] = "{{.Next}}"
 
-                        tmpl, _ := template.ParseFiles(path)
+                        tmpl, _ := template.New("template").Parse(tpl)
 
                         file, _ := os.Create(path)
                         defer file.Close()
@@ -150,7 +231,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
 
                 vars["Next"] = result
 
-                tmpl, _ := template.ParseFiles(path)
+                tmpl, _ := template.New("template").Parse(tpl)
 
                 file, _ := os.Create(path)
                 defer file.Close()
@@ -168,7 +249,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
 
                 vars["Next"] = result
 
-                tmpl, _ := template.ParseFiles(path)
+                tmpl, _ := template.New("template").Parse(tpl)
 
                 file, _ := os.Create(path)
                 defer file.Close()
@@ -186,7 +267,7 @@ func HTMLInject(text string, isLast bool, a string, path string, isFirst bool) {
 
                 vars["Next"] = result
 
-                tmpl, _ := template.ParseFiles(path)
+                tmpl, _ := template.New("template").Parse(tpl)
 
                 file, _ := os.Create(path)
                 defer file.Close()
